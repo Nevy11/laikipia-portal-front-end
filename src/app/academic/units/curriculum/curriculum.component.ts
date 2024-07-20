a0f257cfece9082714b690792fe7888772a9504e
nevy11 <smongare2003@gmail.com>
2024-07-20 10:12:04 +0300

    finish registration of units

--- src/app/academic/units/curriculum/curriculum.component.ts
+++ src/app/academic/units/curriculum/curriculum.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'hinv-curriculum',
   templateUrl: './curriculum.component.html',
-  styleUrl: './curriculum.component.scss'
+  styleUrl: './curriculum.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CurriculumComponent {
-
+  readonly panelOpenState = signal(false);
 }
--- src/app/academic/units/curriculum/curriculum.module.ts
+++ src/app/academic/units/curriculum/curriculum.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatExpansionModule } from '@angular/material/expansion';
 import { CurriculumRoutingModule } from './curriculum-routing.module';
 import { CurriculumComponent } from './curriculum.component';
 
-
 @NgModule({
-  declarations: [
-    CurriculumComponent
-  ],
-  imports: [
-    CommonModule,
-    CurriculumRoutingModule
-  ]
+  declarations: [CurriculumComponent],
+  imports: [CommonModule, CurriculumRoutingModule, MatExpansionModule],
 })
-export class CurriculumModule { }
+export class CurriculumModule {}
